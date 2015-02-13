f97eabb18c84304c8126f603d1daaa25e4c42f0f
vagrant <me@example.com>
2015-02-13 22:05:53 +0000

    created basic structure for site

---
+++ app/views/home.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>rhgksrua</title>
+	<link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+	{{ HTML::style('/resources/css/reset.css') }}
+	{{ HTML::style('/resources/css/home.css') }}
+</head>
+<body>
+	<div class="container">
+		<!-- Start of header: contains title and links to resources -->
+		<header>
+			<div class="header">
+				<h1 class="below-divider">rhgksrua</h1>
+			</div>
+			<div class="resources">
+				<ul class="resources-list">
+					<li class="resource">
+						<a href="">blog</a>
+					</li>
+					<li class="resource">
+						<a href="">Twitter</a>
+					</li>
+					<li class="resource">
+						<a href="">Facebook</a>
+					</li>
+					<li class="resource">
+						<a href="">Github</a>
+					</li>
+				</ul>
+			</div>
+		</header>
+		<!-- End of header -->
+		<div class="content-container">
+			<div class="projects">
+				<h2>Projects</h2>
+				<ul class="project-list">
+					<li class="project">
+						<a href="">Project One</a>
+						<span>Description of project one</span>
+					</li>
+					<li class="project">
+						<a href="">Project Two</a>
+						<span>Description of project two</span>
+					</li>
+					<li class="project">
+						<a href="">Project Three</a>
+						<span>Description of project three</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<footer>
+			<div class="footer-container top-divider">
+				Copyright © 2015 rhgksrua.  All rights reserved.
+			</div>
+		</footer>
+	</div>
+</body>
