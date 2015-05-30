b6bb24745228bc222d656655b944759a8f15a988
Han Koh <rhgksrua@gmail.com>
2015-05-30 22:20:38 +0000

    shows shortened url

--- app/controllers/UrlShortenerController.php
+++ app/controllers/UrlShortenerController.php
@@ -13,14 +13,41 @@ class UrlShortenerController extends BaseController
         $url = filter_var($url, FILTER_SANITIZE_URL);
         $link = new Link;
         $link->url = $url;
-        $link->short_url = $this->createShortUrl($url);
+
+        $short = $this->uniqueId(5);
+
+        $link->short_url = $short;
         $link->visits = 0;
         $link->save();
-        return View::make('urlshortener.short')->with('url', $url);
+        return View::make('urlshortener.short')->with('url', $short);
     }
 
-    private function createShortUrl($url)
+    private function uniqueId($length)
     {
-        return $url;
+        $id = $this->randomId($length);
+
+        $exists = Link::where('short_url', '=', $id)->first();
+        if (!$exists) {
+            return $id;
+        } else {
+            return $this->uniqueId($length);
+        }
+
+    }
+
+    /**
+     * Returns random id
+     * @param  [int]
+     * @return [string]
+     */
+    private function randomId($length)
+    {
+        $alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $id = '';
+        for ($i = 0; $i < $length; $i++) {
+            $index = rand(0, strlen($alpha) - 1);
+            $id .= $alpha[$index];
+        }
+        return $id;
     }
 }
--- app/views/urlshortener/short.blade.php
+++ app/views/urlshortener/short.blade.php
@@ -1,1 +1,19 @@
-<h1>{{ $url }}</h1>
+@extends('master')
+
+
+@section('header')
+    {{ HTML::style('/resources/css/urlshortener/main.css') }}
+@stop
+
+@section('content')
+
+
+
+    <h1><a href='/urlshortener'>URL Shortener</a></h1>
+    <div class='shortener-container'>
+    	<p>Shortened URL</p>
+    	<p><a href='/urlshortener/{{ $url }}'>{{ Request::root() }}/urlshortener/{{ $url }}</a></p>
+
+    </div>
+
+@stop
